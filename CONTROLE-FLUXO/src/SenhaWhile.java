2321a6b3dca2f29abb4f68fa69cf7fde7b257048
Marcio Santos <marciosanthos97@gmail.com>
2024-08-12 17:55:05 -0300

    mudanças para verificação no número de tentativas

--- CONTROLE-FLUXO/src/SenhaWhile.java
+++ CONTROLE-FLUXO/src/SenhaWhile.java
@@ -5,17 +5,24 @@ public class SenhaWhile {
         Scanner senha = new Scanner(System.in);
         String senhaCorreta = "123456";
         String senhaDigitada = "";
+        int tentativas = 0;
+        int maxTentativas = 3;
 
-        while (!senhaDigitada.equals(senhaCorreta)) {
+
+        while (!senhaDigitada.equals(senhaCorreta)&& tentativas < maxTentativas) {
             System.out.println("Digite a senha: ");
             senhaDigitada = senha.nextLine();
-            if (!senhaDigitada.equals(senhaCorreta)) {
-                System.out.println("Sua senha está incorreta, por favor, inseririr senha correta.");
-                
+            tentativas ++;
+
+            if (!senhaDigitada.equals(senhaCorreta)&& tentativas< maxTentativas) {
+                System.out.println("Sua senha está incorreta, por favor, inseririr senha correta.");     
             }
-                  
         }
+        if (senhaDigitada.equals(senhaCorreta)){
         System.out.println("Parabéns, seu acesso foi concedido!");
+        }else{
+        System.out.println("Máximo de tentativas excedido. Acesso não permitido.");
+        }
         senha.close();
     }
 }
